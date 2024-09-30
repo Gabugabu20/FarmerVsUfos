91a36590f345f83cceda64e29844113f9f853200
Gabugabu20 <gabrieleferrali20@gmail.com>
2024-09-30 13:45:15 +0200

    feat: Added Gameover conditiona and screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -7,7 +8,7 @@ using UnityStandardAssets.Characters.FirstPerson;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private UIDocument mainUI;
-    [SerializeField] private UIDocument endGameUI; 
+    [SerializeField] private UIDocument endGameUI;
     [SerializeField] private UIDocument pauseUI;
     [SerializeField] private GameBGMusic backgroundMusic;
 
@@ -27,6 +28,8 @@ public class GameManager : MonoBehaviour
     private float remainingTime;
     private bool isGameRunning = false;
     private bool isGamePaused = false;
+
+    private List<AudioSource> pausedAudioSources = new List<AudioSource>();
 
     void Start()
     {
@@ -88,6 +91,12 @@ public class GameManager : MonoBehaviour
                 isGameRunning = false;
                 EndLevel();
             }
+
+            if (CowManager.Instance.GetCows().Count == 0)
+            {
+                isGameRunning = false;
+                EndLevel();
+            }
         }
 
         if (Keyboard.current.escapeKey.wasPressedThisFrame)
@@ -114,12 +123,13 @@ public class GameManager : MonoBehaviour
             fpsController.DisableInput();
         }
 
-        if(weapon != null)
+        if (weapon != null)
         {
             weapon.SetCanShoot(false);
         }
 
         backgroundMusic.PauseMusic();
+        PauseAllAudio();
 
         mainUI.rootVisualElement.style.display = DisplayStyle.None;
         pauseUI.rootVisualElement.style.display = DisplayStyle.Flex;
@@ -137,15 +147,39 @@ public class GameManager : MonoBehaviour
             fpsController.EnableInput();
         }
 
-        if(weapon != null)
+        if (weapon != null)
         {
             weapon.SetCanShoot(true);
         }
 
         backgroundMusic.ResumeMusic();
+        ResumeAllPausedAudio();
 
         mainUI.rootVisualElement.style.display = DisplayStyle.Flex;
         pauseUI.rootVisualElement.style.display = DisplayStyle.None;
+    }
+
+    public void PauseAllAudio()
+    {
+        pausedAudioSources.Clear();
+        AudioSource[] allAudioSources = FindObjectsOfType<AudioSource>();
+
+        foreach (var audioSource in allAudioSources)
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Pause();
+                pausedAudioSources.Add(audioSource);
+            }
+        }
+    }
+
+    public void ResumeAllPausedAudio()
+    {
+        foreach (var audioSource in pausedAudioSources)
+        {
+            audioSource.UnPause();
+        }
     }
 
     private void UpdateUI()
@@ -167,19 +201,30 @@ public class GameManager : MonoBehaviour
         endGameUI.rootVisualElement.style.display = DisplayStyle.Flex;
 
         int cowsRemaining = CowManager.Instance.GetCows().Count;
-        string resultText = "Well Done partner!\n You were a real Cowboy and showed these stupid UFOs what you were made of!\n";
-        if(cowsRemaining == 16)
+        string resultText = "";
+        if (cowsRemaining == 0)
         {
-            resultText += "HOLY MOLY! You saved all 16 cows!\nNow this is what I call SKILL!";
-        } else if(cowsRemaining > 10)
+            resultText = "GAME OVER!\n The UFOs were too fast for you... but don't give up and try to save your cows again!";
+        }
+        else
         {
-            resultText += $"GOLLY! You saved {cowsRemaining} cows!\nCouldn’t save ‘em all, but hey, that's a whole herd!";
-        } else if(cowsRemaining > 1)
-        {
-            resultText += $"You saved {cowsRemaining} cows!\nNot bad, but I bet you can milk that score even higher!";
-        } else
-        {
-            resultText += "Well, you saved just 1 cow...\nGuess that’s one lonely moo for this Christmas...";
+            resultText = "Well Done partner!\n You were a real Cowboy and showed these stupid UFOs what you were made of!\n";
+            if (cowsRemaining == 16)
+            {
+                resultText += "HOLY MOLY! You saved all 16 cows!\nNow this is what I call SKILL!";
+            }
+            else if (cowsRemaining > 10)
+            {
+                resultText += $"GOLLY! You saved {cowsRemaining} cows!\nCouldn’t save ‘em all, but hey, that's a whole herd!";
+            }
+            else if (cowsRemaining > 1)
+            {
+                resultText += $"You saved {cowsRemaining} cows!\nNot bad, but I bet you can milk that score even higher!";
+            }
+            else
+            {
+                resultText += "Well, you saved just 1 cow...\nGuess that’s one lonely moo for this Christmas...";
+            }
         }
 
         resultTextLabel.text = resultText;
